a654b10cca5124a91cd5b9a6ba70346f81849600
VDevOps85 <105859175+VDevOps85@users.noreply.github.com>
2022-05-19 17:15:23 +0530

    program for basic string operations
    Print the strings in uppercase, lowercase and reverse

---
+++ string_operations.py
@@ -0,0 +1,11 @@
+str1 = input("Enter a lowercase string: ")
+#Print it in Uppercase
+print str1.upper()
+
+str2 = input("Enter an uppercase string: ")
+#print it in Lowercase
+print str2.lower()
+
+#reverse the str1 and str2
+print str1[::-1]
+print str2[::-1]
